651268db0209f78231eaa8bd79ca6adec0b93acb
shaoli-lu <shaoli_lu@yahoo.com>
2022-12-03 12:12:37 -0600

    wiki on click

--- wen.js
+++ wen.js
@@ -1,6 +1,7 @@
 let pic = document.getElementById("usd");
 function submitForm() {
-    document.forms["wen"].submit();
+    // document.forms["wen"].submit();
+    window.open('https://en.wikipedia.org/wiki/Laetiporus', '_blank');
 }
 pic.addEventListener('click', submitForm);
 
